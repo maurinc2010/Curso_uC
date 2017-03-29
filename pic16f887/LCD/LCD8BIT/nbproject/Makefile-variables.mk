b1d455f51ee02c2fb166c9cbf1467f44372538c2
maurinc2010 <marlon.moreno@uptc.edu.co>
2017-03-28 20:46:52 -0500

    LCD modo 8 bits

---
+++ pic16f887/LCD/LCD8BIT/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LCD8BIT.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LCD8BIT.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lcd8bit.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lcd8bit.tar
