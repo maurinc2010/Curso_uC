69cea8013f390136cecca257e91c434493e1d90d
maurinc2010 <marlon.moreno@uptc.edu.co>
2017-04-05 22:17:59 -0500

    Interrupciones Timer 0

--- pic16f887/INTERRUPCIONES/main.asm
+++ pic16f887/INTERRUPCIONES/main.asm
@@ -18,6 +18,8 @@ INTE_VECT CODE 0x0004
     GOTO    INTER
 
 CBLOCK 0X20
+ SW
+ SS
  REG1
  REG2
  REG3
@@ -27,7 +29,8 @@ CBLOCK 0X20
  REG7
  REG8
  FLAG
- LONG  
+ LONG 
+
 ENDC
  
     
@@ -44,18 +47,23 @@ START
     CLRF    ANSELH
     BANKSEL TRISE
     CLRF    TRISC
+    CLRF    TRISD
     MOVLW   0XFF
     MOVWF   TRISB
-    MOVLW   B'10010000'
+    MOVLW   B'10110000'
     MOVWF   INTCON
-    MOVLW   B'00000000'
+    MOVLW   B'00000111'
     MOVWF   OPTION_REG
     BANKSEL PORTC
     CLRF    PORTC
     CLRF    REG6
+    MOVLW   0X0A
+    MOVWF   TMR0
+    MOVLW   0XAA
    
 CC
-    CALL    RETARDO
+    
+    ;MOVWF   PORTD
     GOTO    CC
     
   
@@ -64,12 +72,53 @@ CC
 ;.............................................
 INTER
     BCF	    INTCON,7
+    ;---SE SALVA W Y STATUS
+    MOVWF   SW
+    SWAPF   STATUS,W
+    MOVWF   SS
+    ;----------------
+    BTFSS   INTCON,1;   SI LA INT SE PRODUCE POR RB0
+    BTFSS   INTCON,2
+    GOTO    RBO
+    GOTO    TMR
+RBO
     BCF	    INTCON,1
-    INCF    REG6,F
+    INCF    REG6,F 
     MOVF    REG6,W
-    MOVWF    PORTC
+    MOVWF   PORTC
+    GOTO    RET
+    
+TMR 
+    BCF	    INTCON,2
+    MOVLW   0X04
+    MOVWF   REG4
+    MOVF    REG6,W
+    MOVWF   REG3
+    CALL    MULTI
+    MOVF    REG3,W
+    MOVWF   PORTD
+    MOVLW   0X0A	;TMR0=0.25 SEGUNDOS
+    MOVWF   TMR0
+    CLRF    REG6
+    GOTO    RET
+    
+;....RESTAURA W Y STATUS......
+RET
+    SWAPF   SS,W
+    MOVWF   STATUS
+    MOVF    SW,W
     BSF	    INTCON,7
     RETFIE
+    
+MULTI
+	    MOVF   REG3,W
+	    DECF   REG4,F
+ESTADO	    
+	    ADDWF   REG3,F
+	    DECF    REG4,F
+	    BTFSS   STATUS,Z
+	    GOTO    $-3
+	    RETURN
 ;----------RUTINA DE RETARDO---------------
 RETARDO		MOVLW	.2
 		MOVWF	REG3
