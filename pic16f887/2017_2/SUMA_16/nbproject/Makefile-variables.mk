85df4a2e05b392aeac650a9624f79b0f9e815abc
GAME <game@GAME.GAME>
2017-08-10 11:46:15 -0500

    Suma y resta a 16 bits

---
+++ pic16f887/2017_2/SUMA_16/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SUMA_16.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SUMA_16.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=suma16.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/suma16.tar
--- pic16f887/2017_2/star_asm.X/main.asm
+++ pic16f887/2017_2/star_asm.X/main.asm
@@ -1,16 +1,34 @@
 ; TODO INSERT CONFIG CODE HERE USING CONFIG BITS GENERATOR
-    
-    STATUS	    EQU H'0003'
-    FSR		    EQU H'0004'
-    PORTA	    EQU H'0005'
-    PORTB	    EQU H'0006'
-    PORTC	    EQU H'0007'
-    PORTD	    EQU H'0008'
-    PORTE	    EQU H'0009'
-    PCLATCH	    EQU H'000A'
-    INTCON	    EQU H'000B'
-    TRISB	    EQU H'0086'
-    TRISD	    EQU	H'0088'	 
+list	p=16f887
+INCLUDE	"p16f887.inc"   
+;    STATUS	    EQU H'0003'
+;    FSR		    EQU H'0004'
+;    PORTA	    EQU H'0005'
+;    PORTB	    EQU H'0006'
+;    PORTC	    EQU H'0007'
+;    PORTD	    EQU H'0008'
+;    PORTE	    EQU H'0009'
+;    PCLATCH	    EQU H'000A'
+;    INTCON	    EQU H'000B'
+;    TRISB	    EQU H'0086'
+;    TRISD	    EQU	H'0088'	 
+;_CONFIG1 EQU H'2007'
+ 
+    ; CONFIG1
+ ;__config 0xFBF2    3FFA       3FF7                       3FFF
+ __CONFIG _CONFIG1, _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_OFF & _FCMEN_ON & _LVP_ON
+; CONFIG2
+ ;__config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+ 
+CBLOCK 0X20
+ A_BAJO
+ A_ALTO
+ B_BAJO
+ B_ALTO
+ C_ALTO	;ALMACENA EL RESULTADO
+ C_BAJO
+ ENDC
 
 RES_VECT  CODE    0x0000            ; processor reset vector
     GOTO    START                   ; go to beginning of program
@@ -26,19 +44,88 @@ START
     ;BCF	    TRISB,2
     CLRF    TRISB
     CLRF    TRISD
+    CLRF    TRISC
     BCF	    STATUS,5
     BSF	    PORTB,2
+    BANKSEL ANSELH
+    CLRF    ANSELH
+    BANKSEL PORTB
+    CLRF    PORTD	;PARTE BAJA DE LA CUENTA DE 16 BITS
+    CLRF    PORTC	;PARTE ALTA DE LA CUENTA DE 16 BITS
+ 
+ 
     
-  ;OPERACIONES DE MOVIEMIENTO
-    MOVLW   0XAA	;MOVER DE MEMORIA DE PROGRAMA AL ACUMULADOR
-    MOVWF   PORTB	;MUEVE DEL ACUMULADOR AL PUERTO B
-    MOVF    PORTB,W	;MUEVE EL VALOR DE PORTB AL ACUMULADOR
+
+; CARGAR VALORES A LOS REGITROS DE 16 BITS
+    ;CARGAMOS A =59200
+    MOVLW   0xE7
+    MOVWF   A_ALTO
+    MOVLW   0x40 ;0X4B
+    MOVWF   A_BAJO
+    ;CARGAMOS B=3658
+    MOVLW   0x0E
+    MOVWF   B_ALTO
+    MOVLW   0x4A
+    MOVWF   B_BAJO
+    ;RESULTADO SUMA F58A
+    CALL    SUMA_16
+    CALL    CARGAR_PORT
+    ;RESULTADO RESTA D8F6
+    CALL    RESTA_16
+    CALL    CARGAR_PORT
+     GOTO $                          ; loop forever
+  
+SUMA_16
+    MOVF    B_BAJO,W 
+    ADDWF   A_BAJO,W
+    MOVWF   C_BAJO
+    BTFSS   STATUS,C
+    GOTO    NO_CERO
+    ;CARRY =1
+    INCF    A_ALTO,W
+    ;MOVLW   0X01
+    ;ADDWF   A_ALTO,W
+    ADDWF   B_ALTO,W
+    MOVWF   C_ALTO
+    ;GOTO    FIN
+    RETURN
+    ;PARTE PARA CUANDO CARRY=0
+NO_CERO
+    MOVF    A_ALTO,W
+    ADDWF   B_ALTO,W
+    MOVWF   C_ALTO
+    RETURN
+;FIN   
     
-    COMF    PORTB,F
-    CLRW    
-    COMF    PORTB,W
+CARGAR_PORT
+    ;CARGA RESULTADO DE SUMA EN LOS PUERTOS C,D
+    MOVF    C_ALTO,W
+    MOVWF   PORTC
+    MOVF    C_BAJO,W
     MOVWF   PORTD
+    RETURN
     
-    GOTO $                          ; loop forever
+RESTA_16
+    MOVF    B_BAJO,W 
+    SUBWF   A_BAJO,W
+    MOVWF   C_BAJO
+    BTFSC   STATUS,Z  ;SE CAMBIA EL VALOR DEL BIT DE TESTEO
+    GOTO    NO_CERO_1
+    ;CARRY =0
+    DECF    A_ALTO,F
+    MOVF    B_ALTO,W
+    ;ADDWF   A_ALTO,W
+    SUBWF   A_ALTO,W
+    MOVWF   C_ALTO
+    ;GOTO    FIN
+    RETURN
+    ;PARTE PARA CUANDO CARRY=1
+NO_CERO_1
+    MOVF    B_ALTO,W
+    SUBWF   A_ALTO,W
+    MOVWF   C_ALTO
+    RETURN
+    
+   
 
     END
